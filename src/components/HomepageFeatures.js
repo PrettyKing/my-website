b12c75c58c3640e4e44aee05ee36b8add76b9c06
chalee <36904775+PrettyKing@users.noreply.github.com>
2024-01-03 14:13:08 +0800

    fix: style

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,34 +1,28 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./HomepageFeatures.module.css";
 
 const FeatureList = [
   {
-    title: '学习',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>书山有路勤为径</>
-    ),
+    title: "学习",
+    Svg: require("../../static/img/undraw_docusaurus_mountain.svg").default,
+    description: <>书山有路勤为径</>,
   },
   {
-    title: '生活',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>诗和远方不可缺</>
-    ),
+    title: "生活",
+    Svg: require("../../static/img/undraw_docusaurus_tree.svg").default,
+    description: <>诗和远方不可缺</>,
   },
   {
-    title: '笔记',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>习惯决定着成败</>
-    ),
+    title: "笔记",
+    Svg: require("../../static/img/undraw_docusaurus_react.svg").default,
+    description: <>习惯决定着成败</>,
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
       </div>
